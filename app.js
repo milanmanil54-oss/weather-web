1f2a7c2d1be9fab32815ed2091165734bb1defb0
milan m anil <milanmanil54@gmail.com>
2026-02-13 13:04:15 +0530

    search input and button are functionable

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const apiKey = "c66814fff80a4ad608d630b8c84fb737";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
 
   console.log(data);
@@ -12,4 +16,7 @@ async function checkWeather() {
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
-checkWeather();
+
+searchBtn.addEventListener("click", ()=> {
+  checkWeather(searchBox.value);
+})
