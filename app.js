395e196bd3b69f30508889ea0faf7b1ac1acd99b
milan m anil <milanmanil54@gmail.com>
2026-02-13 14:15:07 +0530

    error message added

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+if(response.status == 404){
+  document.querySelector(".error").style.display = "block";
+  document.querySelector(".weather").style.display = "none";
+  
+}else
+{
   var data = await response.json();
 
   console.log(data);
@@ -37,6 +44,11 @@ async function checkWeather(city) {
   else if (weatherMain === "Snow"){
     weatherIcon.src = "images/snow.png"
   }
+  document.querySelector(".weather").style.display = "block";
+  document.querySelector(".error").style.display = "none";
+}
+
+  
   
 }
 
